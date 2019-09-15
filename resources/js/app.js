33fb898f89fe2692c0297b1fdc23e4814e02d0fc
Takatoshi Hino <like.the.wind0306@gmail.com>
2019-09-15 13:04:21 +0900

    コンポーネント描画

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
+// ルーティング定義import
+import router from './router'
+// ルートコンポーネントimport
+import App from './App.vue'
 
 new Vue({
   el: '#app',
-  template: '<h1>Hello world</h1>'
+  router, // ルーティング定義読み込み
+  components: { App }, // ルートコンポーネントの使用宣言
+  template: '<App />' // ルートコンポーネント描画
 })
