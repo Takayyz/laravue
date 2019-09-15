7cbda88ab496276220e223555ce2137aad4a052c
Takatoshi Hino <like.the.wind0306@gmail.com>
2019-09-15 12:53:55 +0900

    ルーティング設定

---
+++ resources/js/router.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+// ページコンポーネントをインポート
+import PhotoList from './pages/PhotoList.vue'
+import Login from './pages/Login.vue'
+
+// VueRouterプラグインを使用
+// これによって<RouterView />コンポーネントなどを使うことができる
+Vue.use(VueRouter)
+
+// パスとコンポーネントのマッピング
+const routes = [
+  {
+    path: '/',
+    component: PhotoList
+  },
+  {
+    path: '/login',
+    component: Login
+  }
+]
+
+// VueRouterインスタンスを作成
+const router = new VueRouter({
+  routes
+})
+
+// VueRouterインスタンスをエクスポートする
+// app.jsでインポートするため
+export default router
