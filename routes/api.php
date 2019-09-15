17cb16b1d5c64786ecfd75f818428c3d170f5ff7
Takatoshi Hino <like.the.wind0306@gmail.com>
2019-09-15 14:57:31 +0900

    RegisterAPIテスト実装

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// 会員登録
+Route::post('/register', 'Auth\RegisterController@register')->name('register');
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
