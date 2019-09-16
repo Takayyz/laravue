213ef3eabc0b61c4f50373799491bfd494d43f4e
Takatoshi Hino <like.the.wind0306@gmail.com>
2019-09-16 15:19:12 +0900

    Vuex導入

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 // ルーティング定義import
 import router from './router'
+// ストアimport
+import store from './store'
 // ルートコンポーネントimport
 import App from './App.vue'
 
 new Vue({
   el: '#app',
   router, // ルーティング定義読み込み
+  store, // ストア読み込み
   components: { App }, // ルートコンポーネントの使用宣言
   template: '<App />' // ルートコンポーネント描画
 })
