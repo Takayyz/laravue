768d5f2aa1c4499dc94b4824f235cd3ec0994e8c
Takatoshi Hino <like.the.wind0306@gmail.com>
2019-09-28 14:46:14 +0900

    バリデーションエラー対策

--- resources/js/store/auth.js
+++ resources/js/store/auth.js
@@ -1,9 +1,10 @@
 import Axios from "axios"
-import { OK } from '../util'
+import { OK, UNPROCESSABLE_ENTITY } from '../util'
 
 const state = {
   user: null,
-  apiStatus: null
+  apiStatus: null,
+  loginErrorMessages: null
 }
 
 const getters = {
@@ -17,6 +18,9 @@ const mutations = {
   },
   setApiStatus (state, apiStatus) {
     state.apiStatus = apiStatus
+  },
+  setLoginErrorMessages (state, messages) {
+    state.loginErrorMessages = messages
   }
 }
 
@@ -36,7 +40,11 @@ const actions = {
     }
 
     context.commit('setApiStatus', false)
-    context.commit('error/setCode', response.status, { root: true })
+    if (response.status === UNPROCESSABLE_ENTITY) {
+      context.commit('setLoginErrorMessages', response.data.errors)
+    } else {
+      context.commit('error/setCode', response.status, { root: true })
+    }
   },
   async logout (context) {
     const response = await axios.post('/api/logout')
